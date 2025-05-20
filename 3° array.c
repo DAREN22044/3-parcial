e7b9d14fb62a226fe97924b5596a791fd5c9cb69
DAREN22044 <darendoyelmirandabarraza@gmail.com>
2025-05-20 08:54:45 -0600

    Add files via upload

---
+++ 3° array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   int array[10]={1,2,3,4,5,6,7,8,9,0}; 
+   int len=sizeof(array)/sizeof(int); 
+   printf("los bytes del arreglo son: %d\n", sizeof(array));
+   printf("cada entero tiene: %d bytes \n", sizeof(int)); 
+   printf("El arreglo tiene %d elemento\n", len); 
+   
+   return 0;  
+}
---
+++ 3° ocupa % bytes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   char cadena [10];
+   printf("Un int ocupa %d bytes\n", sizeof(int));
+   printf("Un Char ocupa %d bytes\n",sizeof(char));
+   printf("Un float ocupa %d bytes\n",sizeof(float)); 
+   printf("Un double ocupa %d bytes\n",sizeof(double)); 
+   printf("Un cadena ocupa %d bytes\n", sizeof(cadena));
+   return 0;  
+}
